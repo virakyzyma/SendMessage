7ce4f7560ff0421e6865a8eab5c95ac3d6eb30de
Vira Kyzyma <vera.kyzyma8@gmail.com>
2024-03-19 15:19:14 +0200

    Add project files.

---
+++ SendMessage/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SendMessage
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Message message = new Message(SendMessage);
+            message.SendMessage("Message for you", "Hello world");
+        }
+        public static void SendMessage(string title, string message)
+        {
+            Console.WriteLine($"Message:\nTitle: {title}\nText: {message}");
+        }
+        public class Message
+        {
+            public delegate void MessageHandler(string title, string message);
+            public MessageHandler messageHandler;
+            public Message(MessageHandler messageHandler)
+            {
+                this.messageHandler = messageHandler;
+            }
+            public void SendMessage(string title, string message)
+            {
+                messageHandler?.Invoke(title, message);
+            }
+        }
+    }
+}
